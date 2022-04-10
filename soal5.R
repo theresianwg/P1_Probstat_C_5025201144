724e009304452e8758369f6d83f9dec00aa66d2d
theresianwg <81666422+theresianwg@users.noreply.github.com>
2022-04-10 19:09:56 +0700

    done

--- soal5.R
+++ soal5.R
@@ -5,15 +5,16 @@ lamda  <- 3
   dexp(lamda)
   
 #b
+par(mfrow = c(2,2))
 set.seed(1)
 x10 <- 10
 x100 <- 100
 x1000 <- 1000
 x10000 <- 10000
-  hist(rexp(x10))
-  hist(rexp(x100))
-  hist(rexp(x1000))
-  hist(rexp(x10000))
+  hist(rexp(x10), col = 'blue')
+  hist(rexp(x100), col = 'blue')
+  hist(rexp(x1000), col = 'blue')
+  hist(rexp(x10000), col = 'blue')
 
 #c
 lamba  <- 3
