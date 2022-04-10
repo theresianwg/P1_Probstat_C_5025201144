4fcfbd12575895bffe0d2b10ff604943f0046b1d
theresianwg <81666422+theresianwg@users.noreply.github.com>
2022-04-10 19:13:33 +0700

    done

--- soal6.R
+++ soal6.R
@@ -21,3 +21,8 @@ mean <- 50
 sd   <- 8
 
   hist(rnorm(data, mean, sd), breaks = 50, main="5025201144_Theresia Nawangsih_D_DNhistogram")
+
+#c
+sd <- 8
+  miu = sd * sd
+  paste("Nilai Rataan dan Varian adalah ", miu)
